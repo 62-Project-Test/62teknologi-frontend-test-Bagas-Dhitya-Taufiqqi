7f623aaf1a2612a41b3ecc128f5ab718af183e47
bagasdhitya98 <bagas.dhitya@alterra.id>
2024-01-04 11:15:05 +0700

    add: slicing for detail business

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const Slideshow = ({ photos }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -16,30 +17,46 @@ const Slideshow = ({ photos }) => {
   };
 
   return (
-    <div className="relative">
-      {photos?.length > 0 && (
-        <>
-          <img
-            src={photos[currentSlide]}
-            alt={`Slide ${currentSlide + 1}`}
-            className="object-cover w-full h-64 rounded-lg"
-          />
-          <div className="absolute top-1/2 -translate-y-1/2 flex justify-between w-full">
-            <button
-              onClick={goToPrevious}
-              className="bg-gray-800 text-white py-2 px-4 rounded-l-lg"
-            >
-              Previous
-            </button>
-            <button
-              onClick={goToNext}
-              className="bg-gray-800 text-white py-2 px-4 rounded-r-lg"
-            >
-              Next
-            </button>
+    <div className="relative overflow-hidden w-full rounded-md">
+      <motion.div
+        className="flex"
+        style={{ width: `${photos?.length}00%` }}
+        initial={{ x: `-${currentSlide}00%` }}
+        animate={{ x: `-${currentSlide}00%` }}
+        transition={{ type: "tween", duration: 0.5 }}
+      >
+        {photos?.map((photo, index) => (
+          <div
+            key={index}
+            className="w-96 h-96 flex-shrink-0"
+            style={{ minWidth: "100%", display: "inline-block" }}
+          >
+            <motion.img
+              src={photo}
+              width={980}
+              className="h-96 mx-24"
+              alt={`Slide ${index + 1}`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            />
           </div>
-        </>
-      )}
+        ))}
+      </motion.div>
+      <div className="absolute top-1/2 -translate-y-1/2 flex justify-between w-full">
+        <button
+          onClick={goToPrevious}
+          className="bg-gray-800 text-white py-2 px-4 rounded-l-lg"
+        >
+          Previous
+        </button>
+        <button
+          onClick={goToNext}
+          className="bg-gray-800 text-white py-2 px-5 rounded-r-lg"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/detail_business/index.jsx
+++ src/pages/detail_business/index.jsx
@@ -1,14 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useLocation } from "react-router-dom";
 
 import Slideshow from "../../components/Slideshow";
+import ReviewUser from "../../components/ReviewUser";
+import Maps from "../../components/Maps";
 
 const DetailBusiness = () => {
+  const accessToken = import.meta.env.VITE_YELP_TOKEN;
+  const location = useLocation();
+  const id = location?.state?.id;
+  const [detail, setDetail] = useState();
+  const [review, setReview] = useState();
+  const [coordinates, setCoordinates] = useState({ longitude: 0, latitude: 0 });
+
+  const getDetail = async (id) => {
+    try {
+      const response = await axios.get(`${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      setDetail(response?.data);
+      setCoordinates({
+        ...coordinates,
+        longitude: response?.data?.coordinates?.longitude,
+      });
+      setCoordinates({
+        ...coordinates,
+        latitude: response?.data?.coordinates?.latitude,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getReview = async (id) => {
+    try {
+      const response = await axios.get(`${id}/reviews`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      setReview(response?.data?.reviews);
+      console.log(response?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDetail(id);
+    getReview(id);
+  }, []);
+
+  console.log(coordinates);
+
   return (
     <section className="w-screen h-full flex flex-col justify-center items-center">
-      <div className="absolute left-10 top-32">
+      <div className="absolute left-40 top-32">
         <p className="font-semibold text-xl">Detail Business</p>
-        <Slideshow />
+        <div className="my-10">
+          <Slideshow photos={detail?.photos} />
+        </div>
+        <div className="gap-y-5 flex flex-col">
+          <p className="font-semibold">{detail?.name}</p>
+          <div className="gap-y-1 flex flex-col">
+            <p>
+              {detail?.location?.address1}, {detail?.location?.city}
+            </p>
+            <p>Phone Number : {detail?.phone}</p>
+            <p>See on Google Maps : </p>
+            <Maps
+              longitude={coordinates?.longitude}
+              latitude={coordinates?.latitude}
+            />
+          </div>
+        </div>
+        <div className="my-10">
+          <h2 className="text-xl font-semibold mb-4">Reviews</h2>
+          {review?.map((review, index) => (
+            <ReviewUser key={index} review={review} />
+          ))}
+        </div>
       </div>
     </section>
   );
